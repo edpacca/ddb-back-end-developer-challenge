95858c78d4e05805cfed01d9c64ea9ea740ce08a
Eddie Pace <eddie@pace.me.uk>
2024-12-19 13:50:40 -0800

    init jest with ts

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/tests"],
+
+    // use ts-jest for file transformation
+    transform: {
+      "^.+\\.ts?$": "ts-jest",
+    },
+    testRegex: "((\\.|/)(test))\\.tsx?$",
+    moduleFileExtensions: ["ts", "js", "json", "node"],
+  };
