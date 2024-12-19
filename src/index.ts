5c18d17e5b0de3cf163c9799849ca75461c70573
Eddie Pace <eddie@pace.me.uk>
2024-12-19 15:46:40 -0800

    add routes to app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import helmet from "helmet";
 import config from "./config";
+import routes from "./routes/routes";
 
 const app = express();
 
+// configure middleware
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// configure routes
+routes(app);
 
 app.listen(config.port, () => {
   if (config.env === "development") {
