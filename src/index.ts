6a7dc0fa0135249a205d01df0f3c863620fb3318
Eddie Pace <eddie@pace.me.uk>
2024-12-20 13:44:56 -0800

    use morgan for logging in development

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import helmet from "helmet";
+import morgan from "morgan";
 import config from "./config";
 import DB from "./db";
 import routes from "./routes/routes";
-import { initialiseCharacterDb } from "./controllers/characterController";
+import { initialiseCharacterDb } from "./controllers/character/characterController";
 
 const app = express();
 
 // configure middleware
 app.use(helmet()); // helmet sets security related HTTP headers
 app.use(express.json()); // only accept json payloads
+
+if (config.env === "development") {
+  app.use(morgan("dev"));
+}
 
 // Configure DB events
 DB.on("error", console.error.bind(console, "Connection error"));
