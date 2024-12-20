cd2825d21cf861a7a2cee6d3c022122bfbc00159
Eddie Pace <eddie@pace.me.uk>
2024-12-20 13:44:41 -0800

    add endpoints to routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,16 +1,78 @@
 import { Express, Response } from "express";
-import { applyDamageToCharacter } from "../controllers/damageController";
-import { validateDamageRequest } from "../middleware/validateDamageRequest";
-import { DamageRequestBody } from "./types";
+import {
+  validateDamageRequest,
+  validateHealRequest,
+  validateUpdateTempHitPointsRequest,
+} from "../middleware/validation/requestValidation";
+import { healCharacter } from "../controllers/heal/healController";
+import { damageCharacter } from "../controllers/damage/damageController";
+import { updateCharacterTempHitpoints } from "../controllers/tempHitPoints/tempHitPointsController";
+import {
+  DamageRequestBody,
+  EmptyRequestBody,
+  HealRequestBody,
+  UpdateCharacterRequestBody,
+  UpdateTempHitPointsRequestBody,
+} from "./types";
+import {
+  deleteCharacter,
+  getCharacter,
+  updateCharacter,
+} from "../controllers/character/characterController";
 
 function appRouter(app: Express): void {
   app.post(
-    "/damage",
-    validateDamageRequest, // middleware validates incoming data
+    "/characters/:id/damage",
+    validateDamageRequest, // custom middleware validates incoming data
     async (req: DamageRequestBody, res: Response) => {
-      return applyDamageToCharacter(req, res);
+      return damageCharacter(req, res);
     },
   );
+
+  app.post(
+    "/characters/:id/heal",
+    validateHealRequest, // custom middleware validates incoming data
+    async (req: HealRequestBody, res: Response) => {
+      return healCharacter(req, res);
+    },
+  );
+
+  // POST not PUT as might not actually update if current TempHP is higher than request value
+  app.post(
+    "/characters/:id/temphp",
+    validateUpdateTempHitPointsRequest, // custom middleware validates incoming data
+    async (req: UpdateTempHitPointsRequestBody, res: Response) => {
+      return updateCharacterTempHitpoints(req, res);
+    },
+  );
+
+  /**
+   * @route GET /characters/:id
+   * @desc Get a character by ID
+   * @access Public
+   */
+  app.get("/characters/:id", async (req: EmptyRequestBody, res: Response) => {
+    await getCharacter(req, res);
+  });
+
+  // EJP: No validation on character data due to time contstraints and scope
+  /**
+   * @route PUT /characters/:id
+   * @desc Update a character by ID
+   * @access Public
+   */
+  app.put("/characters/:id", async (req: UpdateCharacterRequestBody, res: Response) => {
+    await updateCharacter(req, res);
+  });
+
+  /**
+   * @route DELETE /characters/:id
+   * @desc Delete a character by ID
+   * @access Public
+   */
+  app.delete("/characters/:id", async (req: EmptyRequestBody, res: Response) => {
+    await deleteCharacter(req, res);
+  });
 }
 
 export default appRouter;
--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,11 +1,31 @@
 import { Request } from "express";
+import { Character } from "../model/interface/character";
+
+export interface DamageRequest {
+  damageAmount: number;
+  damageType: string;
+}
+
+export interface HealRequest {
+  healAmount: number;
+}
+
+export interface UpdateTempHitPointsRequest {
+  tempHitPointAmount: number;
+}
 
 export interface TypedRequestBody<T> extends Request {
   body: T;
 }
 
-export type DamageRequestBody = TypedRequestBody<{
-  characterName: string;
-  damageAmount: number;
-  damageType: string;
-}>;
+export type DamageRequestBody = TypedRequestBody<DamageRequest>;
+
+export type HealRequestBody = TypedRequestBody<HealRequest>;
+
+// EJP: verbose, but I avoid using acronyms in function names
+export type UpdateTempHitPointsRequestBody = TypedRequestBody<UpdateTempHitPointsRequest>;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export type EmptyRequestBody = TypedRequestBody<{}>;
+
+export type UpdateCharacterRequestBody = TypedRequestBody<{ data: Character }>;
