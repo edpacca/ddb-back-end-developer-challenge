cc26c7cd5e6bcc60631bf31e7f627e905c649578
Eddie Pace <eddie@pace.me.uk>
2024-12-20 20:33:59 -0800

    rfactor tempHP controller

--- src/controllers/tempHitPoints/tempHitPointsController.ts
+++ src/controllers/tempHitPoints/tempHitPointsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import CharacterDb from "../../model/schema/CharacterSchema";
 import { Character } from "../../model/interface/character";
+import { calcNewTempHitPoints } from "./calcNewTempHitPoints";
 
 export async function updateCharacterTempHitpoints(req: Request, res: Response): Promise<Response> {
   try {
@@ -8,25 +9,26 @@ export async function updateCharacterTempHitpoints(req: Request, res: Response):
     const { tempHitPointsAmount } = req.body;
 
     // use .lean() to strip additional mongodb document properties
-    const character: Character | null = await CharacterDb.findById(id).lean();
+    // use conditional chaining in case of null value
+    const character: Character | null = await CharacterDb.findById(id)?.lean();
 
     if (!character) {
       return res.status(404).json({ message: "Character not found" });
     }
 
     const originalTempHitPoints = character.tempHitPoints;
-    const newTemptHitPoints = Math.max(originalTempHitPoints, tempHitPointsAmount);
+    const updatedTempHitPoints = calcNewTempHitPoints(originalTempHitPoints, tempHitPointsAmount);
 
-    const updatedCharacter: Character = { ...character, tempHitPoints: newTemptHitPoints };
+    const updatedCharacter: Character = { ...character, tempHitPoints: updatedTempHitPoints };
     await CharacterDb.findByIdAndUpdate(id, { ...updatedCharacter });
 
     // return only relevant data
     return res.status(200).json({
       id: character._id,
       name: character.name,
-      tempHitPointsGiven: tempHitPointsAmount,
-      originalTempHitPoints: originalTempHitPoints,
-      newTemptHitPoints: newTemptHitPoints,
+      temp_hit_points_applied: tempHitPointsAmount,
+      original_temp_hit_points: originalTempHitPoints,
+      updated_temp_hit_points: updatedTempHitPoints,
     });
   } catch (error) {
     return res.status(500).json({ message: "Something went terribly wrong...", error });
