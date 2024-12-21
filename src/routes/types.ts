a3fd1d54379d125d047029c2ee53cf11e6b3202f
Eddie Pace <eddie@pace.me.uk>
2024-12-21 00:00:36 -0800

    add create char request type

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -29,3 +29,5 @@ export type UpdateTempHitPointsRequestBody = TypedRequestBody<UpdateTempHitPoint
 export type EmptyRequestBody = TypedRequestBody<{}>;
 
 export type UpdateCharacterRequestBody = TypedRequestBody<{ data: Character }>;
+
+export type CreateCharacterRequestBody = TypedRequestBody<{ data: Character }>;
