d9cd924260c6f238cf4f7c1cb0c762f650fd20a3
Eddie Pace <eddie@pace.me.uk>
2024-12-26 20:08:00 -0800

    only set up DB if in prod

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,5 @@
 import config from "../config";
 import { connectDB, initialiseCharacterDb } from "./dbConnection";
-import { CharacterRepository } from "./localCharacterRepository";
 
 async function setupDatabase() {
   if (config.env === "production") {
@@ -10,10 +9,8 @@ async function setupDatabase() {
       DB.on("error", console.error.bind(console, "Connection error"));
       DB.on("connected", initialiseCharacterDb);
     }
-  } else {
-    new CharacterRepository();
-    console.log("Set up local Character repository");
   }
+  // If not production we use our local repository set up via the interface layer
 }
 
 export default setupDatabase;
